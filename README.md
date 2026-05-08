75db725b6f6be7117e451ecc1a03ecb41cc489a4
JoseOrdonezB <ord231329@uvg.edu.gt>
2026-05-07 19:05:40 -0600

    video demostración

---
+++ README.md
@@ -0,0 +1,2 @@
+# Laboratorio No. 3
+Video demostración: https://youtu.be/_tcnS4Ugw0w
--- ejemplos.py
+++ ejemplos.py
@@ -2,7 +2,7 @@ EPSILON = "ε"
 
 GRAMATICAS_EJEMPLO = {
     "1": {
-        "nombre": "Expresiones aritméticas LL(1)",
+        "nombre": "Expresiones aritméticas con precedencia",
         "simbolo_inicial": "E",
         "gramatica": {
             "E": [["T", "E'"]],
@@ -11,7 +11,42 @@ GRAMATICAS_EJEMPLO = {
             "T": [["F", "T'"]],
             "T'": [["*", "F", "T'"], ["/", "F", "T'"], [EPSILON]],
 
-            "F": [["(", "E", ")"], ["id"]],
+            "F": [["(", "E", ")"], ["id"], ["num"]],
+        },
+    },
+
+    "2": {
+        "nombre": "Sentencias tipo if, while y bloques",
+        "simbolo_inicial": "S",
+        "gramatica": {
+            "S": [
+                ["if", "E", "then", "S", "S'"],
+                ["while", "E", "do", "S"],
+                ["begin", "L", "end"],
+                ["id", ":=", "E"],
+            ],
+
+            "S'": [["else", "S"], [EPSILON]],
+
+            "L": [["S", "L'"]],
+
+            "L'": [[";", "S", "L'"], [EPSILON]],
+
+            "E": [["id"], ["num"], ["true"], ["false"]],
+        },
+    },
+
+    "3": {
+        "nombre": "Múltiples epsilons y propagación de FOLLOW",
+        "simbolo_inicial": "S",
+        "gramatica": {
+            "S": [["A", "B", "C"]],
+
+            "A": [["a", "A"], [EPSILON]],
+
+            "B": [["b", "B"], [EPSILON]],
+
+            "C": [["c", "C"], ["d"], [EPSILON]],
         },
     },
 }
